107bc208642aa2abe8e9c98de9bd22d48b98977c
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2023-08-19 09:39:57 +0800

    removed sample variable references

--- Runtime/References/VariableReferences/FloatReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace SVT.References.VariableReferences
-{
-    [CreateAssetMenu(menuName = "References/Variables/Float Reference")]
-    public class FloatReference : VariableReference<float>
-    {
-    }
-}
--- Runtime/References/VariableReferences/StringReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace SVT.References.VariableReferences
-{
-    [CreateAssetMenu(menuName = "References/Variables/String Reference")]
-    public class StringReference : VariableReference<string>
-    {
-    }
-}
